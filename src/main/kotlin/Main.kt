776b6e738e34e93c597cb8bf1fc96977fac26a5b
Felix Nüsse <felix.nuesse@t-online.de>
2024-06-02 19:21:07 +0200

    properly handle past times. Dont use the year as an indicator, but calculate date ranges.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,17 +4,17 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.terminal
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.default
+import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
+import com.github.ajalt.clikt.parameters.types.int
 import com.github.ajalt.mordant.terminal.YesNoPrompt
 import kotlinx.datetime.*
 import kotlinx.datetime.format.byUnicodePattern
 import org.apache.commons.io.FileUtils
 import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
 import java.util.ArrayList
 import java.util.HashMap
 
@@ -29,11 +29,13 @@ class SignalBackupPurge : CliktCommand(printHelpOnEmptyArgs = true, help = helpS
         const val KEY_ALL = "all"
     }
 
-    private val delete: Boolean? by option("-d", "--delete").flag(default = false).help("Immediately delete Files.")
-    private val dry: Boolean? by option("-n", "--dry-run").flag(default = false).help("Print all files that would be deleted by -d.")
-    private val yes: Boolean? by option("-y", "--yes").flag(default = false).help("Answer all promts with yes")
-    private val printDeletes: Boolean? by option("-p", "--print-deletes").flag(default = false).help("Print a list of shell commands to purge the signal backup folder")
-    private val stats: Boolean? by option("-s", "--stats").flag(default = false).help("print statistics of the purge")
+    private val delete: Boolean by option("-d", "--delete").flag(default = false).help("Immediately delete Files.")
+    private val dry: Boolean by option("-n", "--dry-run").flag(default = false).help("Print all files that would be deleted by -d.")
+    private val yes: Boolean by option("-y", "--yes").flag(default = false).help("Answer all promts with yes")
+    private val printDeletes: Boolean by option("-p", "--print-deletes").flag(default = false).help("Print a list of shell commands to purge the signal backup folder")
+    private val stats: Boolean by option("-s", "--stats").flag(default = false).help("print statistics of the purge")
+    private val keep: Int by option("-k", "--keep").int().default(6).help("How many months should be kept fully")
+    private val keepSecondary: Int by option("-c", "--keep-secondary").int().default(3).help("How many months should have two files after the full ones?")
     private val source by argument().file().default(File("."))
 
 
@@ -48,8 +50,10 @@ class SignalBackupPurge : CliktCommand(printHelpOnEmptyArgs = true, help = helpS
             }
         }
 
-        val currentMoment: Instant = Clock.System.now()
-        val now = currentMoment.toLocalDateTime(TimeZone.UTC)
+        val tzSystem = TimeZone.currentSystemDefault()
+
+        val cutoffPrimary: Instant = Clock.System.now().minus(keep, DateTimeUnit.MONTH, tzSystem)
+        val cutoffSecondary: Instant = Clock.System.now().minus(keep+keepSecondary, DateTimeUnit.MONTH, tzSystem)
 
         // Year: [Month: List<Names>]}
         val orderedMap = HashMap<String, HashMap<String, ArrayList<String>>>()
@@ -64,10 +68,11 @@ class SignalBackupPurge : CliktCommand(printHelpOnEmptyArgs = true, help = helpS
 
             val assertedMap = orderedMap["${date.year}"]!!
 
-            val key = if(date.year == now.year) {
+
+            val key = if(cutoffPrimary.toEpochMilliseconds() < date.toInstant(tzSystem).toEpochMilliseconds()) {
                 KEY_ALL
             } else {
-                "${date.month}"
+                "${date.month.number}"
             }
 
             if(!assertedMap.containsKey(key)) {
@@ -80,8 +85,11 @@ class SignalBackupPurge : CliktCommand(printHelpOnEmptyArgs = true, help = helpS
         val keepList = ArrayList<String>()
         orderedMap.forEach { (year, hashMap) ->
             run {
+                //println("#####")
+                //println(year)
                 hashMap.forEach { (month, files) ->
                     run {
+                        //println(month)
                         if(month==KEY_ALL) {
                             keepList.addAll(files)
                             return@run
@@ -90,25 +98,36 @@ class SignalBackupPurge : CliktCommand(printHelpOnEmptyArgs = true, help = helpS
                         val sorted = files.sorted()
                         val first = sorted.first()
 
+
                         // last year, add two per month
-                        if(year.toInt() == now.year-1) {
+                        val fileDate = createDateFromYearAndMonth(year.toInt(), month.toInt()).toInstant(tzSystem).toEpochMilliseconds()
+                        val isAfterPrimaryStoragePhase = cutoffPrimary.toEpochMilliseconds() > fileDate
+                        val isAfterSecondaryStoragePhase = cutoffSecondary.toEpochMilliseconds() > fileDate
+                        val isInSecondaryStoragePhase = isAfterPrimaryStoragePhase and !isAfterSecondaryStoragePhase
+
+                        if(isInSecondaryStoragePhase) {
                             if(files.size <= 3){
                                 keepList.addAll(files)
                             } else {
                                 keepList.add(first)
-                                if(year.toInt() == now.year-1) {
-                                    val middle = (sorted.size/2)-1 // we start with 0
-                                    keepList.add(sorted[middle])
-                                }
+                                val middle = (sorted.size/2)-1 // we start with 0
+                                keepList.add(sorted[middle])
                             }
-                        } else {
-                            // in every older year, only add one per month.
+                        }
+
+                        if(isAfterSecondaryStoragePhase) {
                             keepList.add(first)
                         }
                     }
                 }
             }
         }
+
+
+        keepList.sorted().forEach {
+            println("Keep: ${source.absoluteFile}/$it")
+        }
+
 
         val discardList = ArrayList<String>()
         validFileList.forEach {
@@ -166,5 +185,9 @@ class SignalBackupPurge : CliktCommand(printHelpOnEmptyArgs = true, help = helpS
         val formatPattern = "yyyy-MM-dd-HH-mm-ss"
         val dateTimeFormat = LocalDateTime.Format { byUnicodePattern(formatPattern) }
         return dateTimeFormat.parse(dateString)
+    }
+
+    private fun createDateFromYearAndMonth(year: Int, month: Int): LocalDateTime {
+        return LocalDateTime(year, month, 1, 0, 0, 0, 0)
     }
 }
